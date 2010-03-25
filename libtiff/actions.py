b12f2c631207c9376d9b615a89ed20a40b8c9e07
aydantasdemir <aydan.tasdemir@markafoni.com>
2010-03-25 11:16:35 +0000

    pspes.xml actions.py ve translations.xml dosyaları eklendi

---
+++ libtiff/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = 'tiff-3.9.2'
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dodoc("ChangeLog", "COPYRIGHT", "TODO", "README*")
+    pisitools.dohtml("html/index.html")
+
